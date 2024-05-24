259593ae17602efd76850d585649ae7d9d32f865
Aditya Vyavahare <vvaditya23@icloud.com>
2024-05-24 13:19:57 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aditya Vyavahare on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
